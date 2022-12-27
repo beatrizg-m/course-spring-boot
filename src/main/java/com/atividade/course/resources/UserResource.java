7126acb9b6c0a254bb33688fce2a873c8614fe96
beatriz <beatrizg.m@yahoo.com>
2022-12-27 11:48:28 -0300

    H2 database, test profile, JPA

--- src/main/java/com/atividade/course/resources/UserResource.java
+++ src/main/java/com/atividade/course/resources/UserResource.java
@@ -1,10 +1,19 @@
 package com.atividade.course.resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.atividade.course.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
     
+    @GetMapping
+    ResponseEntity<User> findAll(){
+        User u = new User(0, "roberto", "roberto@gmail.com", "9999999", "abacaxi");
+        return ResponseEntity.ok().body(u);
+    }
 }
